2494184860ffe75b42add98e2d70637e7f4ca7da
7daytheory <matthewjlowe11@gmail.com>
2018-09-20 10:37:42 -0400

    React Router Fundamentals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
-import Header from './components/layout/Header';
 import AddContact from './components/contacts/addContact';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import { Provider } from './context';
 
@@ -16,8 +18,12 @@ class App extends Component {
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">
-              <AddContact />
-              <Contacts />
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
             </div>
           </div>
         </Router>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = props => {
   const { branding } = props;
@@ -14,9 +15,19 @@ const Header = props => {
 
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <a href="/" className="nav-link">
-            Home
-          </a>
+          <Link to="/" className="nav-link">
+            <i clasName="fas fa-home">Home</i>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact/add" className="nav-link">
+            <i className="fas fa-plus">Add</i>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">
+            <i className="fas fa-question">About</i>
+          </Link>
         </li>
       </ul>
     </nav>
